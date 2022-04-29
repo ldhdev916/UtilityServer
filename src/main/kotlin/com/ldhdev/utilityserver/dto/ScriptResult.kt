08106c906cb7fbeba46a806e030723ff8ee28435
dongheon <dongheon0916@naver.com>
2022-04-30 00:01:21 +0900

    Change failure message due to android toast length limit

--- src/main/kotlin/com/ldhdev/utilityserver/dto/ScriptResult.kt
+++ src/main/kotlin/com/ldhdev/utilityserver/dto/ScriptResult.kt
@@ -4,6 +4,6 @@ data class ScriptResult(val result: String, val success: Boolean) {
     companion object {
         fun success(result: String) = ScriptResult(result, true)
 
-        fun failure(throwable: Throwable) = ScriptResult(throwable.toString(), false)
+        fun failure(throwable: Throwable) = ScriptResult(throwable.message.toString(), false)
     }
 }
